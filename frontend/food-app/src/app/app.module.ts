8507275c94295ec0188b25ba8c9a16a49217a41a
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-04-14 23:25:08 +0200

    app structure changes

---
+++ frontend/food-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module'; 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    SearchBarComponent,
+    SearchBoxComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]), // Set up your root routing configuration here
+    AppRoutingModule, // Import your AppRoutingModule for feature modules and lazy loading
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
