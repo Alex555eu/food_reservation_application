392f6627eb68102afc6f4d27e93021d649677f21
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-04-15 10:51:03 +0200

    init spring setup

---
+++ backend/fod-app-spring-boot/src/main/java/com/app/fodappspringboot/FodAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.app.fodappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FodAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FodAppSpringBootApplication.class, args);
+    }
+
+}
